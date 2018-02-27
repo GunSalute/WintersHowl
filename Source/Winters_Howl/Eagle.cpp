a389c61ac34662ae494b7b41f1357b3d4cd9c9b8
GunSalute <33184284+GunSalute@users.noreply.github.com>
2018-02-27 15:38:06 +0100

    Sheep bug & applied the eagle
    Fixed an issue where the sheep did not move as intented. Added the eagle, with a invisible eagle sight ingame. The eagle can now kill the sheep and the player.

---
+++ Source/Winters_Howl/Eagle.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Eagle.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AEagle::AEagle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh Component");
+	RootComponent = MeshComponent;
+}
+
+// Called when the game starts or when spawned
+void AEagle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEagle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
